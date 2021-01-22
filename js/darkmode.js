c1281299a05da693ad8cbc0a83240d4166e0afa9
laddge <dev.laddge@gmail.com>
2021-01-22 17:50:06 +0900

    add darkmode

---
+++ js/darkmode.js
@@ -0,0 +1,16 @@
+var urlParam = location.search.substring(1);
+
+if(urlParam) {
+  var param = urlParam.split('&');
+  var paramArray = [];
+
+  for (i = 0; i < param.length; i++) {
+    var paramItem = param[i].split('=');
+    paramArray[paramItem[0]] = paramItem[1];
+  }
+
+  if (paramArray.looks == 'dark') {
+    var docbody = document.body;
+    docbody.classList.add('darkmode');
+  }
+}
