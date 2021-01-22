4d94e412d19d998d34d86b3fedb771bfdb8d3cd1
laddge <dev.laddge@gmail.com>
2021-01-22 22:41:54 +0900

    update webhook

--- webhook.php
+++ webhook.php
@@ -1,14 +1,3 @@
 <?php
-$LOG_FILE = dirname(__FILE__).'/hook.log';
-$SECRET_KEY = 'dP$e7U+Lv2!7';
-
-$header = getallheaders();
-$hmac = hash_hmac('sha1', $HTTP_RAW_POST_DATA, $SECRET_KEY);
-if ( isset($header['X-Hub-Signature']) && $header['X-Hub-Signature'] === 'sha1='.$hmac ) {
-    $payload = json_decode($HTTP_RAW_POST_DATA, true);
-    exec('git pull 2>&1');
-    file_put_contents($LOG_FILE, date("[Y-m-d H:i:s]")." ".$_SERVER['REMOTE_ADDR']." git pulled: ".$payload['after']." ".$payload['commits'][0]['message']."\n", FILE_APPEND|LOCK_EX);
-} else {
-    file_put_contents($LOG_FILE, date("[Y-m-d H:i:s]")." invalid access: ".$_SERVER['REMOTE_ADDR']."\n", FILE_APPEND|LOCK_EX);
-}
+  exec('git pull 2>&1');
 ?>
