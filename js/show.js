7ee8eb8f5f63c91f49b806f9787dc4b89541cce2
laddge <dev.laddge@gmail.com>
2021-01-24 12:49:02 +0900

    change js method

--- js/show.js
+++ js/show.js
@@ -1,70 +1,119 @@
 var ver = document.getElementById('version').innerHTML
+var theClass = getClassName();
 
-function getGlobalList(date) {
-  var myClass = '20-1-1';
-  var req = new XMLHttpRequest();
-  req.open('get', 'lists/' + myClass + '/main.csv?ver=' + ver, true);
-  req.send(null);
-  req.onload = function(){
-  	var result = convertCSVtoArray(req.responseText);
-  	var onList = 0;
-  	result.some((row, i) => {
-  	  row.some((element, j) => {
-  	    if (element === date) {
-  	      var day = result[i][1];
-  	      document.getElementById('date').innerHTML = date + '(' + day + ')';
-  	      document.getElementById('output').innerHTML = '';
-        	getClassList(day);
-        	onList = 1;
-        	return true;
-  	    }
-  	  });
-  	});
-  	if (onList == 0) {
-    	document.getElementById('date').innerHTML = date;
-    	document.getElementById('output').innerHTML = 'データが<br>ありません';
-  	}
+function getClassName() {
+  var urlParam = location.search.substring(1);
+  
+  if (urlParam) {
+    var param = urlParam.split('&');
+    var paramArray = [];
+  
+    for (i = 0; i < param.length; i++) {
+      var paramItem = param[i].split('=');
+      paramArray[paramItem[0]] = paramItem[1];
+    }
+  
+    return paramArray.class;
   }
 }
 
-function getClassList(day) {
+function show(date) {
+  var output = document.getElementById('output');
+  var showDate = document.getElementById('showDate');
+  var getMainList = mainList(date);
+  var day = getMainList[0];
+  var specialList = getMainList[1];
+  if (day == 'NotFound') {
+    output.innerHTML = 'クラスが<br>登録されて<br>いません';
+    return
+  }
+  document.getElementById('back').style.display = 'table';
+  document.getElementById('forward').style.display = 'table';
+  if (day == '') {
+    output.innerHTML = 'データが<br>ありません';
+    showDate.innerHTML = date;
+    return;
+  }
+  showDate.innerHTML = date + '(' + day + ')';
   if (day == '休日') {
-    show('休日です。', '');
-    return 0;
+    output.innerHTML = '休日です！';
+    return;
   }
-  var myclass = '20-1-1';
+  if (specialList.length != 0) {
+    var list = specialList;
+  } else {
+    var list = defaultList(day);
+    if (list.length == 0) {
+      output.innerHTML = 'データが<br>ありません';
+      showDate.innerHTML = date;
+      return;
+    }
+  }
+  output.innerHTML = '';
+  list.some((content, num) => {
+    output.innerHTML = output.innerHTML + '<tr><td width=1em>' + (num + 1) + '</td><td>' + content + '</td></tr>';
+  });
+}
+
+function mainList(date) {
   var req = new XMLHttpRequest();
-  req.open('get', 'lists/' + myclass + '/default.csv?ver=' + ver, true);
+  req.open('get', 'lists/' + theClass + '/main.csv?ver=' + ver, false);
   req.send(null);
-  req.onload = function() {
-    var result = convertCSVtoArray(req.responseText);
-    result.some((row, i) => {
-      row.some((element, j) => {
-        if (element === day) {
-          result[i].some((content, cnt) => {
-            if (cnt != 0) {
-              show(content, cnt);
-            }
-          })
-          return true;
-        }
-      })
+  if (req.status == 404) {
+    return ['NotFound', []]
+  }
+  var result = convertCSVtoArray(req.responseText);
+  var cnt;
+  var onList = 0;
+  result.some((row, i) => {
+    row.some((element, j) => {
+      if (element == date) {
+        cnt = i;
+        onList = 1;
+        return true;
+      }
     });
+  });
+  if (onList == 0) {
+    return ['', []];
+  }
+  if (result[cnt].length > 2) {
+    return [result[cnt][1], result[cnt].slice(2)];
+  } else {
+    return [result[cnt][1], []];
   }
 }
 
-function convertCSVtoArray(str){
+function defaultList(day) {
+  var req = new XMLHttpRequest();
+  req.open('get', 'lists/' + theClass + '/default.csv?ver=' + ver, false);
+  req.send(null);
+  var result = convertCSVtoArray(req.responseText);
+  var cnt;
+  var onList = 0;
+  result.some((row, i) => {
+    row.some((element, j) => {
+      if (element == day) {
+        cnt = i;
+        onList = 1;
+        return true;
+      }
+    });
+  });
+  if (onList == 1) {
+    return result[cnt].slice(1);
+  } else {
+    return [];
+  }
+}
+
+function convertCSVtoArray(str) {
   var result = [];
   var tmp = str.split("\n");
-  for(var i=0;i<tmp.length;++i){
+  for (var i = 0; i < tmp.length; ++i) {
     result[i] = tmp[i].split(',');
   }
   return result;
-}
-
-function show(content, cnt) {
-  output = document.getElementById('output');
-  output.innerHTML = output.innerHTML + '<tr><td width=1em>' + cnt + '</td><td>' + content + '</td></tr>';
 }
 
 function getFormatedDate(date) {
@@ -77,25 +126,21 @@ function getFormatedDate(date) {
   return yy + '/' + mm + '/' + dd;
 }
 
-var start = function () {
-  var today = getFormatedDate(new Date());
-  getGlobalList(today);
-}
-
-var forwardDate = function () {
-  var currentDate = ('20' + document.getElementById('date').innerHTML).substr(0, 10); // Only 20XX
+var forwardDate = function() {
+  var currentDate = ('20' + document.getElementById('showDate').innerHTML).substr(0, 10); // Only 20XX
   var date = new Date(currentDate);
   date.setDate(date.getDate() + 1);
-  var nextDay = getFormatedDate(date);
-  getGlobalList(nextDay);
+  var theDay = getFormatedDate(date);
+  show(theDay);
 }
 
-var backDate = function () {
-  var currentDate = ('20' + document.getElementById('date').innerHTML).substr(0, 10); // Only 20XX
+var backDate = function() {
+  var currentDate = ('20' + document.getElementById('showDate').innerHTML).substr(0, 10); // Only 20XX
   var date = new Date(currentDate);
   date.setDate(date.getDate() - 1);
-  var nextDay = getFormatedDate(date);
-  getGlobalList(nextDay);
+  var theDay = getFormatedDate(date);
+  show(theDay);
 }
 
-start();
+var today = getFormatedDate(new Date());
+show(today);
